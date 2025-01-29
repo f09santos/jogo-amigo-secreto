c405c607ac3d3c9fe1fee15d2a7a28e4c87ffa0c
Fabio Santos <f09santos@gmail.com>
2025-01-29 16:45:36 -0300

    definação do sorteio

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+let arrayAmigos = [];
+let listaDeAmigos = document.querySelector('#listaAmigos');
+let resultado = document.querySelector("#resultado");
+
+
+// Adicionar o nome digitado dentro do array amigos
+ function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('#amigo');
+    if(nomeAmigo.value === '') {
+         alert('Favor inserir um nome para o sorteio')
+         return
+     }
+     arrayAmigos.push(nomeAmigo.value);
+     nomeAmigo.value = '';
+     atualizarLista()
+    }
+// Atualizar a lista de Amigos
+    function atualizarLista() {
+       listaDeAmigos.innerHTML= '';
+       for(i = 0; i < arrayAmigos.length; i++) {
+        let nome = document.createElement('li');
+       nome.textContent = arrayAmigos[i];
+        listaDeAmigos.appendChild(nome);
+       }
+    }
+// Função para sortear um amigo
+    function sortearAmigo() {
+        if(arrayAmigos.length < 2) {
+            alert('Atenção! Favor adicionar pelo menos 2 amigos para o sorteio')
+            return;
+        }
+
+        let numeroGerado = parseInt(Math.random() * arrayAmigos.length);
+        resultado.innerHTML = `Atenção! O Amigo(a) sorteado foi: ${arrayAmigos[numeroGerado]}`;
+
+    }
+    
 
 
 
@@ -5,5 +42,4 @@
 
 
 
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
